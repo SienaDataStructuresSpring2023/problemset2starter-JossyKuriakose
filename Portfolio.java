ed76e390e782e8d7d9ce27a4a9e4cfe69adcaadd
JossyKuriakose <j29kuri@siena.edu>
2023-02-16 22:32:31 -0500

    just started this

--- Portfolio.java
+++ Portfolio.java
@@ -2,23 +2,23 @@
 /**
  * Write a description of class Portfolio here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Joe Kuriakose)
+ * @version (2/16/2023)
  */
 public class Portfolio
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+     @Override
+     public String toString()
+     {
+         //DO NOT EDIT THIS METHOD.
+         StringBuffer sb = new StringBuffer();
+         sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                 "Symbol", "Name", "Shares", "@Price", "Total"));
+         sb.append("-----------------------------------------------------------------------\n");
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+         for(StockHolding s : stocks){
+             sb.append(s.toString());
+         }
+         return sb.toString();
+     }
 }
--- StockHolding.java
+++ StockHolding.java
@@ -1,16 +1,81 @@
 /**
  * Write a description of class Asset here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Joe Kuriakose)
+ * @version (2/16/2023)
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+     private String symbol;
+     private String name;
+     private double numShares;
+     private double price;
+     /**
+      * 
+      */
+     public StockHolding(String symbol, String name, double numShares, double price){
+        this.symbol = symbol;
+        this.name = name;
+        this.numShares = numShares;
+        this.price = price;
+        }
+      
+     /**
+      * 
+      */
+     public String getSymbol(){
+        return symbol;
+        }
+     
+     /**
+       * 
+       */
+     public String name(){
+        return name;
+        }
+      
+     /**
+      * 
+      */
+     public double getNumShares(){
+        return numShares;
+        }
+        
+     /**
+      * 
+      */
+     public double getPrice(){
+        return price;
+        }
+     
+     /**
+      * 
+      */
+     public void buyShares(double numberOfShares, double pricePerShare){
+        this.numShares = numShares;
+        this.price = pricePerShare;
+        }
+     
+     /**
+      * 
+      */
+     public double sellShares(double numberOfShares){
+        double sold = 0.0;
+        if(numberOfShares <= numShares){
+         numShares = numShares - numberOfShares;
+         sold = numShares * price;
+        }
+        return sold;
+        }
+        
+     /**
+      * 
+      */
+     @Override
+     public String toString()
+     {
+         //DO NOT EDIT THIS METHOD.
+         return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+         symbol, name, numShares, price, numShares * price); 
+     }
 }
